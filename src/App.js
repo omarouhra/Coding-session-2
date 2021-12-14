cf50b9139a40b1f33d43f74592f7bf33dc5dc873
omarouhra <omarmaroc26@gmail.com>
2021-12-14 11:50:15 +0100

    Init Commit + Taiwlind setup done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1>Welcome To Coding Session 2</h1>;
+  return (
+    <div className="flex  flex-col items-center justify-center h-screen space-y-12">
+      <h1 className="text-4xl">Welcome To Coding Session 2</h1>
+      <ul className="space-y-4 font-bold list-disc">
+        <li>React.js</li>
+        <li>Tailwind.css</li>
+        <li>Atomic Design</li>
+        <li>Deployement using vercel</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
