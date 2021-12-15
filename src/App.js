1b5ddd26a3a6ccc05695513a2c07ef75a6014ab5
omarouhra <omarmaroc26@gmail.com>
2021-12-15 21:41:09 +0100

    built + state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  // Increment Function
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  // Decrement Function
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
   return (
-    <div className="flex  flex-col items-center justify-center h-screen space-y-12">
-      <h1 className="text-4xl">Welcome To Coding Session 2</h1>
-      <ul className="space-y-4 font-bold list-disc">
-        <li>React.js</li>
-        <li>Tailwind.css</li>
-        <li>Atomic Design</li>
-        <li>Deployement using vercel</li>
-      </ul>
+    <div
+      className={
+        counter > 4 ? "container bg-red-500" : "container bg-green-100"
+      }>
+      {/* Alert Card */}
+      <div
+        className={
+          counter > 4
+            ? "alertContainer -translate-y-12 opacity-1"
+            : "alertContainer translate-y-12 opacity-0"
+        }>
+        <p>Due to COVID-19 only groups of 4 are allowed.</p>
+      </div>
+      {/* Card */}
+
+      <div className={counter > 4 ? "card bg-red-400 -translate-y-4  " : "card"}>
+        {/* Top Section */}
+        <div className=' px-12 py-6 flex items-center justify-center h-60 w-[500px] '>
+          {/* Table */}
+          <div className='w-20 h-20 rounded-full bg-white relative'></div>
+        </div>
+
+        {/* Bottom Sectiom */}
+
+        <div className=' px-12 bg-white py-10 flex items-center justify-between rounded-b-md '>
+          <p>For how many people !</p>
+
+          <div className='flex space-x-6 items-center  overflow-hidden'>
+            <button
+              onClick={decrement}
+              className={
+                counter > 1 ? " button translate-y-0" : "button translate-y-8"
+              }>
+              {" < "}
+            </button>
+            <p>{counter}</p>
+            <button
+              onClick={increment}
+              className={counter > 4 ? "button translate-y-8" : "button"}>
+              {">"}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
